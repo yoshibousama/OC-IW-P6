daaaaa627c7c8a15b7b1ee7f9b0bbc16478e36e5
yoshibousama <aomine-sama@outlook.fr>
2023-04-24 16:44:15 +0200

    deleted html gallery

--- FrontEnd/assets/js/scripts.js
+++ FrontEnd/assets/js/scripts.js
@@ -1,30 +1,23 @@
-async function callApi() {
-  const response = await fetch("http://localhost:5678/api/works");
-  const data = await response.json();
-  console.log(data);
+fetch("http://localhost:5678/api/works")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    loadDataWorks(data);
+  });
 
-  function loadDataWorks(dataWorks) {
-    for (let i = 0; i < dataWorks.length; i++) {
-      const gallery = document.querySelector(".gallery");
-      const figureElm = document.createElement("figure");
-      const imgElm = document.createElement("img");
-      imgElm.src = dataWorks[i].imageUrl;
-      const figcaptionElm = document.createElement("figcaption");
-      figcaptionElm.innerText = dataWorks[i].title;
+function loadDataWorks(dataWorks) {
+  for (let i = 0; i < dataWorks.length; i++) {
+    const gallery = document.querySelector(".gallery");
+    const figureElm = document.createElement("figure");
+    const imgElm = document.createElement("img");
+    imgElm.src = dataWorks[i].imageUrl;
+    const figcaptionElm = document.createElement("figcaption");
+    figcaptionElm.innerText = dataWorks[i].title;
 
-      gallery.appendChild(figureElm);
-      figureElm.appendChild(imgElm);
-      figureElm.appendChild(figcaptionElm);
-    }
+    gallery.appendChild(figureElm);
+    figureElm.appendChild(imgElm);
+    figureElm.appendChild(figcaptionElm);
   }
-  loadDataWorks(data);
 }
-callApi();
-
-// fetch("http://localhost:5678/api/works")
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
