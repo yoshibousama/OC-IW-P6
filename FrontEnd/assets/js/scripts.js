332b66d8e6f9a044ccd06f6335e78e772b99094d
yoshibousama <aomine-sama@outlook.fr>
2023-04-26 10:41:49 +0200

    added alt img

--- FrontEnd/assets/js/scripts.js
+++ FrontEnd/assets/js/scripts.js
@@ -13,6 +13,7 @@ function loadDataWorks(dataWorks) {
     const figureElm = document.createElement("figure");
     const imgElm = document.createElement("img");
     imgElm.src = dataWorks[i].imageUrl;
+    imgElm.dataset.title = dataWorks[i].title;
     const figcaptionElm = document.createElement("figcaption");
     figcaptionElm.innerText = dataWorks[i].title;
 
@@ -29,3 +30,19 @@ fetch("http://localhost:5678/api/categories")
   .then(function (data) {
     console.log(data);
   });
+
+const btnAll = document.querySelector(".btn_filter-all");
+const btnId1 = document.querySelector(".btn_filter-id1");
+const btnId2 = document.querySelector(".btn_filter-id2");
+const btnId3 = document.querySelector(".btn_filter-id3");
+
+function galleryReset() {
+  document.querySelector(".gallery").innerHTML = "";
+}
+
+// Filtre pour catégorie objet
+// const boutonAll = document.querySelector(".btn-all");
+
+btnAll.addEventListener("click", function () {
+  galleryReset();
+});
